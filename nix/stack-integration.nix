fb6642e498d8ec550dc4123ae963ea153af678c4
Solene Rapenne <solene.rapenne@tweag.io>
2022-08-11 12:38:28 +0200

    allow stack to build with nix

---
+++ nix/stack-integration.nix
@@ -0,0 +1,19 @@
+# from https://www.tweag.io/blog/2022-06-02-haskell-stack-nix-shell/
+let
+  sources = import ./nix/source.nix
+  pkgs = sources.nixpkgs;
+in
+
+# See https://docs.haskellstack.org/en/stable/nix_integration/#using-a-custom-shellnix-file
+{ ghc }:
+
+pkgs.haskell.lib.buildStackProject {
+  inherit ghc;
+  name = "haskell-stack-nix";
+  # System dependencies needed at compilation time
+  buildInputs = [
+    pkgs.postgresql
+    pkgs.zlib
+  ];
+}
+
