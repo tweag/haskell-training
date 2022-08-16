47cb43246e6918c80a1fc63457473eccd71cabbf
Marco Perone <marco.perone@tweag.io>
2022-08-16 11:32:06 +0200

    add debugging message

--- shell.nix
+++ shell.nix
@@ -19,4 +19,7 @@ in
 pkgs.mkShell {
   buildInputs = [ stack-wrapped ];
   NIX_PATH = "nixpkgs=" + pkgs.path;
+  shellHook = ''
+            test -d ~/.stack/setup-exe-cache && echo "if you have trouble compiling, try to delete ~/.stack/stack-cache-exe/ directory and try again"
+            '';
 }
