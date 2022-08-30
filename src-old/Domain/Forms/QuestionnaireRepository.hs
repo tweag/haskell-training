68361ce0bea8371996cd536335e27b2653e8f0ce
Marco Perone <marco.perone@tweag.io>
2022-08-30 12:01:08 +0200

    remove src-old

--- src-old/Domain/Forms/QuestionnaireRepository.hs
+++
@@ -1,9 +0,0 @@
-module Domain.Forms.QuestionnaireRepository where
-
-import Domain.Forms.Id
-import Domain.Forms.Questionnaire
-
-data QuestionnaireRepository m = QuestionnaireRepository
-  { createNewQuestionnaire  :: Questionnaire -> m (Id Questionnaire)
-  , selectAllQuestionnaires :: m [Questionnaire]
-  }
